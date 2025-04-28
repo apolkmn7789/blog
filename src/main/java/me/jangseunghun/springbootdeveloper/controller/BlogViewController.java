b3673e23b55a92b9bf5c1d14ae5d4ee3eab77f6a
apolkmn7789 <ip01021107076@gmail.com>
2025-04-28 14:01:37 +0900

    글 조회하기

---
+++ src/main/java/me/jangseunghun/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,38 @@
+package me.jangseunghun.springbootdeveloper.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.jangseunghun.springbootdeveloper.domain.Article;
+import me.jangseunghun.springbootdeveloper.dto.ArticleListViewResponse;
+import me.jangseunghun.springbootdeveloper.dto.ArticleViewResponse;
+import me.jangseunghun.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model){
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles",articles); // 블로그 글 리스트 저장
+
+        return "articleList"; // articles.html 뷰 조회
+    }
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+}
--- src/main/java/me/jangseunghun/springbootdeveloper/domain/Article.java
+++ src/main/java/me/jangseunghun/springbootdeveloper/domain/Article.java
@@ -5,7 +5,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/* 생성시간과 수정시간 추가 04/28 */
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,6 +30,14 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate // 엔티티가 생성 될 때 생성 시간 저장
+    @Column(name= "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 엔티티가 수정 될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder // 빌더 패턴으로 객체 생성
     public Article(String title, String content){
