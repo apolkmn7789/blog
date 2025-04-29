9ded5108f7d71d6718cd9dd135dcdd9dc137349e
apolkmn7789 <ip01021107076@gmail.com>
2025-04-29 15:19:40 +0900

    블로그 수정기능 추가

--- src/main/java/me/jangseunghun/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/jangseunghun/springbootdeveloper/controller/BlogViewController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,5 +35,16 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id 는 없을수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id,Model model){
+        if(id == null){ // id 가 없으면
+            model.addAttribute("article",new ArticleViewResponse());// 생성
+        }else{ // 있으면
+            Article article = blogService.findById(id);
+            model.addAttribute("article",new ArticleViewResponse(article)); // 수정
+        }
+        return "newArticle";
     }
 }
